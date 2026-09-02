25343c6fb424d86357cc814f9de623c9ca7923cc
eternalsolutions-agency <ricandsmm@gmail.com>
2026-09-02 10:10:10 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,4 @@
+// Configurazione Supabase - Ciccio Pubblicità
 
+const SUPABASE_URL = "https://onpjrsvkplewzpmudazx.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_bIvZhkRyVp2btqkHwSbIRA_mjVNXXMn";
